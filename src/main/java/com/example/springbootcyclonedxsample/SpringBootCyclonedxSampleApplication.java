ebd18affd5188c5791839bc258d714060f73102e
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-07-25 12:35:28 +0200

    initial commit

---
+++ src/main/java/com/example/springbootcyclonedxsample/SpringBootCyclonedxSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcyclonedxsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCyclonedxSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCyclonedxSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcyclonedxsample/SpringBootCyclonedxSampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootcyclonedxsample;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCyclonedxSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
